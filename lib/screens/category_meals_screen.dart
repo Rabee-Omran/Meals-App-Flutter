362e9354ec5d49c278e5b51b4e4be3eaad254ba3
RabeeOmran <rabeeomran@kali>
2020-10-01 03:42:30 +0300

    Complete Meals Screen

---
+++ lib/screens/category_meals_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/dummy_data.dart';
+import 'package:meals_app/models/meal.dart';
+import 'package:meals_app/widgets/meal_item.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  static const routeName = '/category-meals';
+  // final String categoryId;
+  // final String categoryTitle;
+
+  // const CategoryMealsScreen(this.categoryId, this.categoryTitle);
+  @override
+  Widget build(BuildContext context) {
+    final routeArgs =
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
+    final categoryId = routeArgs['id'];
+    final categoryTitle = routeArgs['title'];
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(categoryTitle),
+        ),
+        body: ListView.builder(
+            itemCount: categoryMeals.length,
+            itemBuilder: (context, index) {
+              final meal = categoryMeals[index];
+              return MealItem(
+                title:meal.title,
+                imageUrl : meal.imageUrl,
+                affordability:meal.affordability,
+                complexity:meal.complexity,
+                duration: meal.duration,
+
+
+              );
+            }));
+  }
+}
